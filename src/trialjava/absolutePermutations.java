e5f554bbb37dfe8f3546b4b0a4e45720e2f41f7f
Love <lovesingh25@gmail.com>
2016-05-23 00:06:46 +0530

    Few changes

--- src/trialjava/absolutePermutations.java
+++ src/trialjava/absolutePermutations.java
@@ -1,6 +1,9 @@
 package trialjava;
 
 
+import javafx.beans.binding.BooleanExpression;
+import javafx.beans.binding.IntegerExpression;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -8,12 +11,13 @@ import java.util.Scanner;
  * Created by Sandhu on 22-05-2016.
  */
 public class absolutePermutations {
+    /*
     private static Boolean calPerm(int n, int k) {
         int temp[] = new int[n + 1];
         for (int i = 1; i < temp.length; i++) {
             temp[i] = i;
         }
-        if (absDifference(temp, k)) {
+       if (absDifference(temp, k)) {
             printArray(temp);
         } else {
             while (findNextPer(temp)) {
@@ -21,6 +25,7 @@ public class absolutePermutations {
                     printArray(temp);
                     return null;
                 }
+                //System.out.println();
             }
             System.out.print("-1");
         }
@@ -61,16 +66,54 @@ public class absolutePermutations {
             System.out.print(temp[i] + " ");
         }
     }
-
-    private static Boolean absDifference(int[] temp, int k) {
-        for (int i = 1; i < temp.length; i++) {
-            if (Math.abs(temp[i] - i) != k) {
+*/
+    private static Boolean absDifference(char[] temp, int k) {
+        for (int i = 0; i < temp.length; i++) {
+            if (Math.abs(Integer.valueOf(String.valueOf(temp[i])) - (i + 1)) != k) {
                 return false;
             }
         }
         return true;
     }
 
+    private static Boolean calPerm2(int n, int k) {
+        String temp = "0";
+        for (int i = 1; i <= n; i++) {
+            temp = temp + i;
+        }
+        int p = Integer.valueOf(temp);
+        Boolean isIt = false;
+        while (String.valueOf(p).toCharArray().length <= n) {
+            isIt = absDifference(String.valueOf(p).toCharArray(), k);
+
+            if (isIt) {
+                char[] c = String.valueOf(p).toCharArray();
+                int i = 0;
+                while (i < c.length) {
+                    System.out.print(c[i] + " ");
+                    i++;
+                }
+                break;
+            }
+            p = p + 9;
+            while (!containsN(String.valueOf(p).toCharArray(), n)) {
+                p = p + 9;
+            }
+        }
+        if (!isIt) {
+            System.out.print("-1");
+        }
+        return null;
+    }
+
+    private static Boolean containsN(char[] a, int n) {
+        for (int i = 0; i < a.length; i++) {
+            if (Integer.valueOf(String.valueOf(a[i])) > n) {
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         int t;
         Scanner in = new Scanner(System.in);
@@ -85,7 +128,7 @@ public class absolutePermutations {
         }
         i = 0;
         while (t > 0) {
-            calPerm(n[i], k[i]);
+            calPerm2(n[i], k[i]);
             System.out.println();
             i++;
             t--;
